64c62f21a1bd5654eec66a9275dd80687387ed00
sam <samyesuraj132@gmail.com>
2026-05-25 07:48:44 +0530

    Implemented pick and place for 3 tools and place inside a basket

--- src/pick_place.cpp
+++ src/pick_place.cpp
@@ -1,80 +1,150 @@
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.hpp>
 
-int main(int argc,char** argv)
+using moveit::planning_interface::MoveGroupInterface;
+
+constexpr double APPROACH_HEIGHT = 0.15;
+constexpr double LIFT_HEIGHT     = 0.15;
+
+void moveToPose(
+    MoveGroupInterface& arm,
+    const geometry_msgs::msg::Pose& pose)
 {
-  rclcpp::init(argc,argv);
+    arm.setPoseTarget(pose);
+    arm.move();
+}
 
-  auto node =
-      std::make_shared<rclcpp::Node>(
-          "xarm_pick_place",
-          rclcpp::NodeOptions()
-              .automatically_declare_parameters_from_overrides(true));
+void openGripper(MoveGroupInterface& gripper)
+{
+    gripper.setNamedTarget("open");
+    gripper.move();
+}
 
-  rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(node);
+void closeGripper(MoveGroupInterface& gripper)
+{
+    gripper.setNamedTarget("close");
+    gripper.move();
+}
 
-  std::thread spinner([&executor](){
-      executor.spin();
-  });
-  
-  moveit::planning_interface::MoveGroupInterface arm(
-      node,
-      "xarm5");
+void pickObject(
+    MoveGroupInterface& arm,
+    MoveGroupInterface& gripper,
+    geometry_msgs::msg::Pose object_pose)
+{
+    auto approach = object_pose;
+    approach.position.z += APPROACH_HEIGHT;
 
-  moveit::planning_interface::MoveGroupInterface gripper(
-      node,
-      "xarm_gripper");
+    // Approach
+    moveToPose(arm, approach);
 
+    // Descend
+    moveToPose(arm, object_pose);
 
-  auto pose = arm.getCurrentPose().pose;
+    // Grab
+    closeGripper(gripper);
 
-  arm.setMaxVelocityScalingFactor(0.2);
-  arm.setMaxAccelerationScalingFactor(0.2);
+    // Lift
+    moveToPose(arm, approach);
+}
 
-  RCLCPP_INFO(node->get_logger(),"OPEN");
+void placeObject(
+    MoveGroupInterface& arm,
+    MoveGroupInterface& gripper,
+    geometry_msgs::msg::Pose basket_pose)
+{
+    auto approach = basket_pose;
+    approach.position.z += APPROACH_HEIGHT;
 
-  gripper.setNamedTarget("open");
-  gripper.move();
+    // Approach basket
+    moveToPose(arm, approach);
 
-  geometry_msgs::msg::Pose target;
-  target = pose;
-  
+    // Descend
+    moveToPose(arm, basket_pose);
 
-  target.position.x += 0.;
-  target.position.y = 0.25;
-  target.position.z += 0;
+    // Release
+    openGripper(gripper);
 
+    // Lift away
+    moveToPose(arm, approach);
+}
 
-  RCLCPP_INFO(node->get_logger(),"MOVE TO PICK");
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
 
-  arm.setPoseTarget(target);
-  arm.move();
+    auto node = std::make_shared<rclcpp::Node>(
+        "xarm_pick_place",
+        rclcpp::NodeOptions()
+            .automatically_declare_parameters_from_overrides(true));
 
-  RCLCPP_INFO(node->get_logger(),"CLOSE");
+    rclcpp::executors::SingleThreadedExecutor executor;
+    executor.add_node(node);
 
-  gripper.setNamedTarget("close");
-  gripper.move();
+    std::thread spinner([&executor]()
+    {
+        executor.spin();
+    });
 
-  target.position.y = -0.25;
+    MoveGroupInterface arm(node, "xarm5");
+    MoveGroupInterface gripper(node, "xarm_gripper");
 
-  RCLCPP_INFO(node->get_logger(),"MOVE TO PLACE");
+    arm.setMaxVelocityScalingFactor(0.2);
+    arm.setMaxAccelerationScalingFactor(0.2);
 
-  arm.setPoseTarget(target);
-  arm.move();
+    openGripper(gripper);
 
-  RCLCPP_INFO(node->get_logger(),"OPEN");
+    //------------------------------------------------------------------
+    // Tool locations
+    //------------------------------------------------------------------
 
-  gripper.setNamedTarget("open");
-  gripper.move();
+    std::vector<geometry_msgs::msg::Pose> tools(3);
 
-  RCLCPP_INFO(node->get_logger(),"HOLD");
+    auto current = arm.getCurrentPose().pose;
 
-  arm.setNamedTarget("hold");
-  arm.move();
+    tools[0] = current;
+    tools[0].position.x = 0.40;
+    tools[0].position.y = 0.25;
 
-  rclcpp::shutdown();
-  spinner.join();
+    tools[1] = current;
+    tools[1].position.x = 0.40;
+    tools[1].position.y = 0.50;
 
-  return 0;
+    tools[2] = current;
+    tools[2].position.x = 0.40;
+    tools[2].position.y = -0.50;
+
+    //------------------------------------------------------------------
+    // Basket location
+    //------------------------------------------------------------------
+
+    geometry_msgs::msg::Pose basket = current;
+    basket.position.x = 0.40;
+    basket.position.y = -0.25;
+
+    //------------------------------------------------------------------
+    // Pick all tools and place into basket
+    //------------------------------------------------------------------
+
+    for (size_t i = 0; i < tools.size(); ++i)
+    {
+        RCLCPP_INFO(
+            node->get_logger(),
+            "Processing tool %ld",
+            static_cast<long>(i + 1));
+
+        pickObject(arm, gripper, tools[i]);
+        placeObject(arm, gripper, basket);
+    }
+
+    //------------------------------------------------------------------
+    // Return home
+    //------------------------------------------------------------------
+    
+    arm.setNamedTarget("hold");
+    arm.move();
+
+    rclcpp::shutdown();
+    spinner.join();
+
+    return 0;
 }
